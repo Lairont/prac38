ea1d28f2389692cc2f05c868711f19f9b0bd20fb
Никита <153456515+Lairont@users.noreply.github.com>
2025-04-21 23:40:35 +0300

    Add files via upload

---
+++ ConsoleApp1/BMW.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BMW : Avto, ITurbo, IEco
+    {
+        private int power;
+        private double fuelConsumption;
+        public int Power { get => power; set => power = value; }
+        public double FuelConsumption { get => fuelConsumption; set => fuelConsumption = value; }
+
+        public BMW(string dop, string model, int speed, int power, double fuelConsumption) : base(model, speed, dop )
+           
+        {
+            Power = power;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine("BMW едет!");
+        }
+
+        public void Turbo()
+        {
+            Console.WriteLine("BMW: Турбонаддув включен!");
+        }
+
+        public void Eco()
+        {
+            Console.WriteLine("BMW: Экономичный режим включен!");
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine($"Мощность двигателя: {Power} л.с.");
+            Console.WriteLine($"Расход топлива: {FuelConsumption} л/100 км");
+        }
+    }
+}
---
+++ ConsoleApp1/Maz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Maz : Avto, ITurbo
+    {
+        public Maz(string model, int speed, string dop)
+            : base(model, speed, dop) { }
+
+        public override void Drive()
+        {
+            Console.WriteLine($"{Model} едет со скоростью {Speed} км/ч.");
+        }
+
+        public void Turbo()
+        {
+            Console.WriteLine("Турбонаддув включен!");
+        }
+    
+    }
+}
